3531d3cfee49621aa731c6a6c0e2220a1e004953
cevilan <cevilan@gmail.com>
2024-02-13 20:26:17 +0100

    config yml
    Añadir fichero de config yml

--- tienda/tienda.py
+++ tienda/tienda.py
@@ -2,7 +2,8 @@
 Módulo tienda
 """
 
-import argparse
+import argparse, yaml
+
 # leemos los parámetros de entrada
 parser = argparse.ArgumentParser()
 parser.add_argument('--servidor', dest='servidor', type=str, required=False, default="localhost",help='IP o nombre el servidor donde se inicia la aplicación')
@@ -10,5 +11,10 @@ parser.add_argument('--puerto', dest='puerto', type=str, required=False, default
 parser.add_argument('--config', dest='config', type=str, required=True, help='Ruta y nombre del fichero de configuración de la aplicación')
 parser.add_argument('--key', dest='key', type=str, required=True, help='Valor del API KEY para consumir servicios de la aplicación almacen')
 args = parser.parse_args()
+
+#datos del fichero de config
+with open("config.yml", 'r') as ymlfile:     
+    cfg = yaml.full_load(ymlfile) 
+
 
 #se debe crear un API_KEY para la aplicación tienda.
