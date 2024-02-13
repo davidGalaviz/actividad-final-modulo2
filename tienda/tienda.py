80659b3f22e2d3e94dc0a00404ad15bb7e6e4e86
cevilan <cevilan@gmail.com>
2024-02-13 19:59:12 +0100

    Update tienda.py

--- tienda/tienda.py
+++ tienda/tienda.py
@@ -1,3 +1,14 @@
 """
 Módulo tienda
 """
+
+import argparse
+# leemos los parámetros de entrada
+parser = argparse.ArgumentParser()
+parser.add_argument('--servidor', dest='servidor', type=str, required=False, default="localhost",help='IP o nombre el servidor donde se inicia la aplicación')
+parser.add_argument('--puerto', dest='puerto', type=str, required=False, default='5000',help='puerto donde se expondrá el API')
+parser.add_argument('--config', dest='config', type=str, required=True, help='Ruta y nombre del fichero de configuración de la aplicación')
+parser.add_argument('--key', dest='key', type=str, required=True, help='Valor del API KEY para consumir servicios de la aplicación almacen')
+args = parser.parse_args()
+
+#se debe crear un API_KEY para la aplicación tienda.
