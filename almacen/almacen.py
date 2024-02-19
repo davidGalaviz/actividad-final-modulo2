f6def7a02d8ece5c5559ec35945388345f8f2648
David <110849310+davidGalaviz@users.noreply.github.com>
2024-02-19 13:11:10 -0600

    almacén: docs SwaggerUI

--- almacen/almacen.py
+++ almacen/almacen.py
@@ -3,10 +3,14 @@ import yaml
 import argparse
 from flask import Flask, request, jsonify, make_response
 from sqlalchemy import create_engine
+from flask_swagger_ui import get_swaggerui_blueprint
 
 from db_models import Base 
 from servicio_almacen import ServicioAlmacen
 from servicio_consumidores import ServicioConsumidores
+
+SWAGGER_URL="/api/docs" # Aquí accederemos al Swagger UI
+API_URL="/services/spec" # Aquí servimos los datos del archivo api_doc.yaml
 
 # Recibimos los parámetros ingresados por el usuario
 parser = argparse.ArgumentParser(
@@ -70,6 +74,17 @@ else:
     # para gestionar los consumidores
     servicio_consumidores = ServicioConsumidores(engine)
 
+    # Configuramos Swagger UI (para la documentación)
+    swagger_ui_blueprint = get_swaggerui_blueprint(
+        SWAGGER_URL,
+        API_URL,
+        config={
+            'app_name': 'JSON Placeholder/Posts'
+        }
+    )
+
+    app.register_blueprint(swagger_ui_blueprint)
+
 try:
     # Hay que verificar que haya un consumidor por default en al archivo config
     nombre_consumidor_admin, api_key_consumidor_admin = config['consumidor_almacen'], config['consumidor_almacen_key']
@@ -91,20 +106,37 @@ except KeyError:
         yaml.dump(datos_consumidor_admin, file)
 
 
+# Hay que servir la información del archivo api_doc.yaml,
+# para que Swagger UI pueda leer la documentación de aquí.
+@app.route("/services/spec")
+def yaml_docs():
+    with open('api_doc.yaml', 'r',  encoding='utf8') as archivo_docs:
+        data = yaml.safe_load(archivo_docs)
+        return(data)
+
+def requiere_api_key(func):
+    func._requiere_api_key = True
+    return func
+
 @app.before_request
 def verificar_api_key():
-    # Todo request al API debe venir acompañado de un API Key válido
+    # Hay varios requests que deben venir acompañados de un API Key válido,
     # bajo el header 'api-key'
-    api_key = request.headers.get('api-key')
-    api_key_es_valida = servicio_consumidores.validar_api_key(api_key, api_key_consumidor_admin) if api_key is not None else False
+    if request.endpoint in app.view_functions:
+        view_func = app.view_functions[request.endpoint]
+        verificar = hasattr(view_func, '_requiere_api_key')
 
-    print(api_key)
-    print(api_key_consumidor_admin)
-    print(api_key_es_valida)
+        if(verificar):
+            api_key = request.headers.get('api-key')
+            api_key_es_valida = servicio_consumidores.validar_api_key(api_key, api_key_consumidor_admin) if api_key is not None else False
 
-    # Si el API Key no es válida o no se especificó, devolvemos un error de "Unauthorized"
-    if not api_key_es_valida:
-        return jsonify({'error': 'Unauthorized'}), 401
+            print(api_key)
+            print(api_key_consumidor_admin)
+            print(api_key_es_valida)
+
+            # Si el API Key no es válida o no se especificó, devolvemos un error de "Unauthorized"
+            if not api_key_es_valida:
+                return jsonify({'error': 'Unauthorized'}), 401
 
 @app.post('/api/registrar-consumidor')
 def registrar_consumidor():
@@ -125,6 +157,7 @@ def registrar_consumidor():
 
 
 @app.get('/api/articulos')
+
 def get_articulos():
     # Devolvemos la lista completa de artículos
     response = jsonify(servicio_almacen.get_articulos())
@@ -133,6 +166,7 @@ def get_articulos():
     return response
 
 @app.get('/api/articulos/<sku>')
+@requiere_api_key
 def get_articulo(sku: str):
     # Devolvemos el artículo con el SKU especificado
     response = jsonify(servicio_almacen.get_articulo_por_sku(sku))
@@ -141,6 +175,7 @@ def get_articulo(sku: str):
     return response
 
 @app.post('/api/articulos')
+@requiere_api_key
 def crear_articulo():
     # Leemos los datos del body del request
     datos_articulo = request.get_json()
@@ -156,6 +191,7 @@ def crear_articulo():
     return response
 
 @app.put('/api/articulos/<sku>')
+@requiere_api_key
 def actualizar_articulo(sku: str):
     # Leemos los datos del body del request
     datos_articulo = request.get_json()
@@ -171,6 +207,7 @@ def actualizar_articulo(sku: str):
     return response
 
 @app.delete('/api/articulos/<sku>')
+@requiere_api_key
 def eliminar_articulo(sku: str):
     # Eliminamos el artículo de la base de datos
     servicio_almacen.eliminar_articulo(sku)
@@ -181,6 +218,7 @@ def eliminar_articulo(sku: str):
     return response
 
 @app.patch('/api/articulos/<sku>/registrar-recepcion')
+@requiere_api_key
 def registrar_recepcion_articulo(sku: str):
     cantidad = request.get_json()['cantidad']
 
@@ -195,6 +233,7 @@ def registrar_recepcion_articulo(sku: str):
     return response
 
 @app.patch('/api/articulos/<sku>/registrar-salida')
+@requiere_api_key
 def registrar_salida_articulo(sku: str):
     cantidad = request.get_json()['cantidad']
 
